1e029a1d93838a01809b32af17fb6d5be26ecbb0
Leonille <leonillematunan@gmail.com>
2023-06-04 16:11:59 -0700

    Styling action page, not done

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,52 +1,53 @@
 import React from 'react'
+import Graph from '../Assets/Photos/pi_graph.png'
 import Image3 from '../Assets/Photos/img_3.jpeg'
 
 function Action() {
   return (
     <div id="action-section">
-      <section className="relative sm:ml-64 flex flex-col justify-center items-center">
+      <section className="relative sm:ml-64 flex flex-col justify-center items-center pb-8">
         <div className="container px-2 py-2 mx-auto flex flex-col">
           <div className="lg:w-5/6 mx-auto">
             <div>
               <h1 className="font-lato font-semibold text-4xl my-4 text-uw-purple">Action</h1>
               {/* Image for School to Prison Piepline 1200x500*/}
               <img alt="content" className="rounded-md lg:w-[1250px] lg:h-[300px] md:w-auto md:h-auto object-cover" src={Image3} />
+              <p className="font-sans text-xs italic mx-2">Image from Massachusetts Advocates for Children</p>
               <h1 className="font-lato text-4xl my-3 text-uw-gold">How can we tackle this?</h1>
             </div>
             <div className="flex flex-col mt-4 mx-2 text-lg sm:px-5">
-                <p className="font-roboto text-justify text-uw-gray mt-3">The School to Prison Pipeline is a system of policies and practices that pushes
-                  students out of school and into the criminal justice system. This system disproportionately affects students of color and students with disabilities.
-                  The School to Prison Pipeline is a system of policies and practices that pushes students out of school and into the criminal justice system. This system disproportionately affects students of color and students with disabilities. The School to Prison Pipeline is a system of policies and practices that pushes students out of school and into the criminal justice system.
-                  This system disproportionately affects students of color and students with disabilities.
-                </p>
-              <div className="flex justify-center items-center flex-col 2xl:flex-row font-roboto my-5">
-                <img
-                  className="w-[450px] h-[450px] object-cover rounded-md md:mr-4 mt-4 sm:mt-0"
-                  src="https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500.jpg"
-                  alt="Placeholder"
-                />
-                <p className="mt-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae nunc congue, posuere justo sed, mattis lectus.
-                  Donec cursus sapien ut tellus venenatis, eget accumsan massa malesuada. Nunc aliquam eleifend lacus, vel varius risus porta at.
-                  Aliquam erat volutpat. Sed commodo, arcu hendrerit fermentum mattis, mauris nibh faucibus lectus, quis pellentesque magna massa eget mauris.
-                  Quisque tincidunt vulputate maximus. Ut metus nunc, placerat id metus ut, ultrices pharetra libero.
-                  Etiam a lacus nec felis posuere lacinia. Ut rhoncus placerat velit. Aenean scelerisque semper arcu nec ultricies.
-                  Mauris iaculis, nulla vitae vehicula dapibus, velit nibh feugiat nisi, fermentum blandit erat justo euismod urna.
-                  Pellentesque dictum tincidunt tortor in lacinia.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae nunc congue, posuere justo sed, mattis lectus.
-                  Donec cursus sapien ut tellus venenatis, eget accumsan massa malesuada. Nunc aliquam eleifend lacus, vel varius risus porta at.
-                  Aliquam erat volutpat. Sed commodo, arcu hendrerit fermentum mattis, mauris nibh faucibus lectus, quis pellentesque magna massa eget mauris.
-                  Quisque tincidunt vulputate maximus. Ut metus nunc, placerat id metus ut, ultrices pharetra libero.
-                  Etiam a lacus nec felis posuere lacinia.
-                </p>
+              <p className="font-roboto text-justify text-uw-gray mt-3">TIn building and sharing this site, we wanted to make an effort to spread awareness of this issue with as many people
+                as possible. It is important that we aim to empower individual students, faculty, parents,
+                and others to explore and research how the school to prison pipeline may affect their local school district, and also help them
+                understand that all of us have the means to band together in collective action in order to make a difference.
+              </p>
+              <div className="flex justify-center items-center flex-col md:flex-row font-roboto my-5">
+                <div className="mx-4">
+                  <img
+                    className="rounded-md md:mr-4 mt-4 sm:mt-0"
+                    src={Graph}
+                    alt="Graph of data"
+                  />
+                  <p className="text-sm md:mt-0">2017-2018 survey on crime & safety from students</p>
+                </div>
+                <div className="mt-4">
+                  <p>
+                    The School To Prison Pipeline is a framework for analysis of a situation, and because of this, the way in which it acts can vary significantly from place to place.
+                    For instance, the students at an expensive private school may almost never come into contact with a pipeline as a result of factors such as economic status
+                    and the level of funding available for the school to take disciplinary action on its own, while the local public high school in an economically
+                    disadvantaged area will struggle with issues like increased likelihood of misbehavior by students and increased police presence. Depending on the popular
+                    politics of the location, the local school board and politicians may be very open to taking action or incredibly hostile to it
+                    <span className="italic font-semibold">
+                      (for instance, people in power may want students who take illegal actions to be expelled regardless of the crime in order to give off a “tough on crime” appearance)
+                    </span>.
+                    This is why it is so important for us as individuals to make an effort to understand what is going on in our local communities
+                    and figuring out what the best course of action is in that specific situation.
+                  </p>
+                </div>
               </div>
               <p className="mt-4 font-roboto">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae nunc congue, posuere justo sed, mattis lectus.
-                Donec cursus sapien ut tellus venenatis, eget accumsan massa malesuada. Nunc aliquam eleifend lacus, vel varius risus porta at.
-                Aliquam erat volutpat. Sed commodo, arcu hendrerit fermentum mattis, mauris nibh faucibus lectus, quis pellentesque magna massa eget mauris.
-                Quisque tincidunt vulputate maximus. Ut metus nunc, placerat id metus ut, ultrices pharetra libero.
-                Etiam a lacus nec felis posuere lacinia. Ut rhoncus placerat velit. Aenean scelerisque semper arcu nec ultricies.
-                Mauris iaculis, nulla vitae vehicula dapibus, velit nibh feugiat nisi, fermentum blandit erat justo euismod urna.
-                Pellentesque dictum tincidunt tortor in lacinia.
+                It is also important that we share and emphasize the importance of this situation with the people around us, in order to encourage and build the will for change in our local communities.
+                All of us have the ability to form movements, taking cues from the leaders before us in our approach. Together, we can help dismantle this pipeline.
               </p>
             </div>
           </div>
